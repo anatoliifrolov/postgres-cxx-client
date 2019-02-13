c104ed57bef491d34a0d1dc1bb72c64af0d84551
afrolov <frolov.public@yandex.ru>
2019-02-13 12:18:17 +0300

    Support all configuration options

--- src/Config.cpp
+++ src/Config.cpp
@@ -30,6 +30,30 @@ Config::Builder& Config::Builder::operator=(Builder&& other) noexcept = default;
 
 Config::Builder::~Builder() = default;
 
+Config::Builder& Config::Builder::application_name(std::string const& val) {
+    return set("application_name", val);
+}
+
+Config::Builder& Config::Builder::client_encoding(std::string const& val) {
+    return set("client_encoding", val);
+}
+
+Config::Builder& Config::Builder::connect_timeout(Duration const val) {
+    return setInterval("connect_timeout", val);
+}
+
+Config::Builder& Config::Builder::dbname(std::string const& val) {
+    return set("dbname", val);
+}
+
+Config::Builder& Config::Builder::fallback_application_name(std::string const& val) {
+    return set("fallback_application_name", val);
+}
+
+Config::Builder& Config::Builder::gsslib(std::string const& val) {
+    return set("gsslib", val);
+}
+
 Config::Builder& Config::Builder::host(std::string const& val) {
     return set("host", val);
 }
@@ -38,24 +62,138 @@ Config::Builder& Config::Builder::hostaddr(std::string const& val) {
     return set("hostaddr", val);
 }
 
-Config::Builder& Config::Builder::port(int const val) {
-    return set("port", val);
+Config::Builder& Config::Builder::keepalives(bool const val) {
+    return val ? enable("keepalives") : disable("keepalives");
 }
 
-Config::Builder& Config::Builder::user(std::string const& val) {
-    return set("user", val);
+Config::Builder& Config::Builder::keepalives_count(int const val) {
+    return setNumber("keepalives_count", val);
+}
+
+Config::Builder& Config::Builder::keepalives_idle(Duration const val) {
+    return setInterval("keepalives_idle", val);
+}
+
+Config::Builder& Config::Builder::keepalives_interval(Duration const val) {
+    return setInterval("keepalives_interval", val);
+}
+
+Config::Builder& Config::Builder::krbsrvname(std::string const& val) {
+    return set("krbsrvname", val);
+}
+
+Config::Builder& Config::Builder::options(std::string const& val) {
+    return set("options", val);
+}
+
+Config::Builder& Config::Builder::passfile(std::string const& val) {
+    return set("passfile", val);
 }
 
 Config::Builder& Config::Builder::password(std::string const& val) {
     return set("password", val);
 }
 
-Config::Builder& Config::Builder::dbname(std::string const& val) {
-    return set("dbname", val);
+Config::Builder& Config::Builder::port(int const val) {
+    return setNumber("port", val);
 }
 
-Config::Builder& Config::Builder::set(std::string const& key, int const val) {
+Config::Builder& Config::Builder::requirepeer(std::string const& val) {
+    return set("requirepeer", val);
+}
+
+Config::Builder& Config::Builder::requiressl(bool const val) {
+    return val ? enable("requiressl") : disable("requiressl");
+}
+
+Config::Builder& Config::Builder::service(std::string const& val) {
+    return set("service", val);
+}
+
+Config::Builder& Config::Builder::sslcert(std::string const& val) {
+    return set("sslcert", val);
+}
+
+Config::Builder& Config::Builder::sslcompression(bool const val) {
+    return val ? enable("sslcompression") : disable("sslcompression");
+}
+
+Config::Builder& Config::Builder::sslcrl(std::string const& val) {
+    return set("sslcrl", val);
+}
+
+Config::Builder& Config::Builder::sslkey(std::string const& val) {
+    return set("sslkey", val);
+}
+
+Config::Builder& Config::Builder::sslmode(SslMode const val) {
+    return set("sslmode", [val] {
+        switch (val) {
+            case SslMode::ALLOW: {
+                return "allow";
+            }
+            case SslMode::DISABLE: {
+                return "disable";
+            }
+            case SslMode::PREFER: {
+                return "prefer";
+            }
+            case SslMode::REQUIRE: {
+                return "require";
+            }
+            case SslMode::VERIFY_CA: {
+                return "verify-ca";
+            }
+            case SslMode::VERIFY_FULL: {
+                return "verify-full";
+            }
+            case SslMode::DEFAULT: {
+                break;
+            }
+        }
+        return "prefer";
+    }());
+}
+
+Config::Builder& Config::Builder::sslrootcert(std::string const& val) {
+    return set("sslrootcert", val);
+}
+
+Config::Builder& Config::Builder::target_session_attrs(TargetSession val) {
+    return set("target_session_attrs", [val] {
+        switch (val) {
+            case TargetSession::ANY: {
+                return "any";
+            }
+            case TargetSession::READ_WRITE: {
+                return "read-write";
+            }
+            case TargetSession::DEFAULT: {
+                break;
+            }
+        }
+        return "any";
+    }());
+}
+
+Config::Builder& Config::Builder::user(std::string const& val) {
+    return set("user", val);
+}
+
+Config::Builder& Config::Builder::disable(std::string const& key) {
+    return set(key, "0");
+}
+
+Config::Builder& Config::Builder::enable(std::string const& key) {
+    return set(key, "1");
+}
+
+Config::Builder& Config::Builder::setNumber(std::string const& key, int val) {
     return set(key, std::to_string(val));
+}
+
+Config::Builder& Config::Builder::setInterval(std::string const& key, Duration val) {
+    return set(key, std::to_string(std::chrono::duration_cast<std::chrono::seconds>(val).count()));
 }
 
 Config::Builder& Config::Builder::set(std::string const& key, std::string const& val) {
--- tests/src/TestConfig.cpp
+++ tests/src/TestConfig.cpp
@@ -1,38 +1,172 @@
+#include <map>
+#include <string>
 #include <gtest/gtest.h>
 #include <postgres/Config.h>
+
+using namespace std::chrono_literals;
 
 namespace postgres {
 
 TEST(TestConfig, Values) {
-    auto const cfg = Config::Builder{}.host("HOST")
-                                      .hostaddr("ADDR")
-                                      .port(1234)
-                                      .user("USER")
-                                      .password("PASS")
-                                      .dbname("NAME")
-                                      .set("application_name", "APP")
-                                      .set("connect_timeout", 2345)
-                                      .build();
-    auto const& keys = cfg.keys();
-    auto const& vals = cfg.values();
-    ASSERT_STREQ(keys[0], "application_name");
-    ASSERT_STREQ(vals[0], "APP");
-    ASSERT_STREQ(keys[1], "connect_timeout");
-    ASSERT_STREQ(vals[1], "2345");
-    ASSERT_STREQ(keys[2], "dbname");
-    ASSERT_STREQ(vals[2], "NAME");
-    ASSERT_STREQ(keys[3], "host");
-    ASSERT_STREQ(vals[3], "HOST");
-    ASSERT_STREQ(keys[4], "hostaddr");
-    ASSERT_STREQ(vals[4], "ADDR");
-    ASSERT_STREQ(keys[5], "password");
-    ASSERT_STREQ(vals[5], "PASS");
-    ASSERT_STREQ(keys[6], "port");
-    ASSERT_STREQ(vals[6], "1234");
-    ASSERT_STREQ(keys[7], "user");
-    ASSERT_STREQ(vals[7], "USER");
-    ASSERT_EQ(keys[8], nullptr);
-    ASSERT_EQ(vals[8], nullptr);
+    auto const c = Config::Builder{}.application_name("APP")
+                                    .client_encoding("ENC")
+                                    .connect_timeout(1min)
+                                    .dbname("DB")
+                                    .fallback_application_name("APP2")
+                                    .gsslib("GSS")
+                                    .host("HOST")
+                                    .hostaddr("ADDR")
+                                    .keepalives(true)
+                                    .keepalives_count(2)
+                                    .keepalives_idle(3min)
+                                    .keepalives_interval(4min)
+                                    .krbsrvname("KRBS")
+                                    .options("OPTS")
+                                    .passfile("PASSF")
+                                    .password("PASSW")
+                                    .port(5)
+                                    .requirepeer("PEER")
+                                    .requiressl(true)
+                                    .service("SVC")
+                                    .sslcert("CERT")
+                                    .sslcompression(true)
+                                    .sslcrl("CRL")
+                                    .sslkey("KEY")
+                                    .sslrootcert("ROOT")
+                                    .user("USER")
+                                    .build();
+    auto const k = c.keys();
+    auto const v = c.values();
+    auto       i = 0;
+    ASSERT_STREQ(k[i], "application_name");
+    ASSERT_STREQ(v[i], "APP");
+    ++i;
+    ASSERT_STREQ(k[i], "client_encoding");
+    ASSERT_STREQ(v[i], "ENC");
+    ++i;
+    ASSERT_STREQ(k[i], "connect_timeout");
+    ASSERT_STREQ(v[i], "60");
+    ++i;
+    ASSERT_STREQ(k[i], "dbname");
+    ASSERT_STREQ(v[i], "DB");
+    ++i;
+    ASSERT_STREQ(k[i], "fallback_application_name");
+    ASSERT_STREQ(v[i], "APP2");
+    ++i;
+    ASSERT_STREQ(k[i], "gsslib");
+    ASSERT_STREQ(v[i], "GSS");
+    ++i;
+    ASSERT_STREQ(k[i], "host");
+    ASSERT_STREQ(v[i], "HOST");
+    ++i;
+    ASSERT_STREQ(k[i], "hostaddr");
+    ASSERT_STREQ(v[i], "ADDR");
+    ++i;
+    ASSERT_STREQ(k[i], "keepalives");
+    ASSERT_STREQ(v[i], "1");
+    ++i;
+    ASSERT_STREQ(k[i], "keepalives_count");
+    ASSERT_STREQ(v[i], "2");
+    ++i;
+    ASSERT_STREQ(k[i], "keepalives_idle");
+    ASSERT_STREQ(v[i], "180");
+    ++i;
+    ASSERT_STREQ(k[i], "keepalives_interval");
+    ASSERT_STREQ(v[i], "240");
+    ++i;
+    ASSERT_STREQ(k[i], "krbsrvname");
+    ASSERT_STREQ(v[i], "KRBS");
+    ++i;
+    ASSERT_STREQ(k[i], "options");
+    ASSERT_STREQ(v[i], "OPTS");
+    ++i;
+    ASSERT_STREQ(k[i], "passfile");
+    ASSERT_STREQ(v[i], "PASSF");
+    ++i;
+    ASSERT_STREQ(k[i], "password");
+    ASSERT_STREQ(v[i], "PASSW");
+    ++i;
+    ASSERT_STREQ(k[i], "port");
+    ASSERT_STREQ(v[i], "5");
+    ++i;
+    ASSERT_STREQ(k[i], "requirepeer");
+    ASSERT_STREQ(v[i], "PEER");
+    ++i;
+    ASSERT_STREQ(k[i], "requiressl");
+    ASSERT_STREQ(v[i], "1");
+    ++i;
+    ASSERT_STREQ(k[i], "service");
+    ASSERT_STREQ(v[i], "SVC");
+    ++i;
+    ASSERT_STREQ(k[i], "sslcert");
+    ASSERT_STREQ(v[i], "CERT");
+    ++i;
+    ASSERT_STREQ(k[i], "sslcompression");
+    ASSERT_STREQ(v[i], "1");
+    ++i;
+    ASSERT_STREQ(k[i], "sslcrl");
+    ASSERT_STREQ(v[i], "CRL");
+    ++i;
+    ASSERT_STREQ(k[i], "sslkey");
+    ASSERT_STREQ(v[i], "KEY");
+    ++i;
+    ASSERT_STREQ(k[i], "sslrootcert");
+    ASSERT_STREQ(v[i], "ROOT");
+    ++i;
+    ASSERT_STREQ(k[i], "user");
+    ASSERT_STREQ(v[i], "USER");
+}
+
+TEST(TestConfig, Manual) {
+    auto const c = Config::Builder{}.set("application_name", "APP")
+                                    .setInterval("connect_timeout", 1min)
+                                    .enable("keepalives")
+                                    .setNumber("keepalives_count", 2)
+                                    .disable("sslcompression")
+                                    .build();
+    auto const k = c.keys();
+    auto const v = c.values();
+    auto       i = 0;
+    ASSERT_STREQ(k[i], "application_name");
+    ASSERT_STREQ(v[i], "APP");
+    ++i;
+    ASSERT_STREQ(k[i], "connect_timeout");
+    ASSERT_STREQ(v[i], "60");
+    ++i;
+    ASSERT_STREQ(k[i], "keepalives");
+    ASSERT_STREQ(v[i], "1");
+    ++i;
+    ASSERT_STREQ(k[i], "keepalives_count");
+    ASSERT_STREQ(v[i], "2");
+    ++i;
+    ASSERT_STREQ(k[i], "sslcompression");
+    ASSERT_STREQ(v[i], "0");
+}
+
+TEST(TestConfig, SslMode) {
+    std::map<SslMode, std::string> const samples{{SslMode::ALLOW,       "allow"},
+                                                 {SslMode::DISABLE,     "disable"},
+                                                 {SslMode::PREFER,      "prefer"},
+                                                 {SslMode::REQUIRE,     "require"},
+                                                 {SslMode::VERIFY_CA,   "verify-ca"},
+                                                 {SslMode::VERIFY_FULL, "verify-full"},
+                                                 {SslMode::DEFAULT,     "prefer"}};
+
+    for (auto const& sample : samples) {
+        auto const c = Config::Builder{}.sslmode(sample.first).build();
+        ASSERT_EQ(sample.second, c.values()[0]);
+    }
+}
+
+TEST(TestConfig, TargetSession) {
+    std::map<TargetSession, std::string> const samples{{TargetSession::ANY,        "any"},
+                                                       {TargetSession::READ_WRITE, "read-write"},
+                                                       {TargetSession::DEFAULT,    "any"}};
+
+    for (auto const& sample : samples) {
+        auto const c = Config::Builder{}.target_session_attrs(sample.first).build();
+        ASSERT_EQ(sample.second, c.values()[0]);
+    }
 }
 
 }  // namespace postgres
