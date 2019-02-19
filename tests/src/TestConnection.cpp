3186c9b77727cfc7a504ff0641f362495666ea7d
afrolov <frolov.public@yandex.ru>
2019-02-19 15:50:46 +0300

    Test Connection

--- tests/src/TestConnection.cpp
+++ tests/src/TestConnection.cpp
@@ -13,19 +13,15 @@ static constexpr auto CONNECT_URI = "postgresql://cxx_client:cxx_client@/cxx_cli
 
 TEST(TestConnection, Ping) {
     ASSERT_EQ(PQPING_OK, Connection::ping(Config::build()));
-    ASSERT_EQ(PQPING_NO_RESPONSE, Connection::ping(Config::Builder{}.port(2345).build()));
-    ASSERT_EQ(PQPING_NO_ATTEMPT, Connection::ping(Config::Builder{}.set("k", "v").build()));
-}
-
-TEST(TestConnection, PingStr) {
     ASSERT_EQ(PQPING_OK, Connection::ping(CONNECT_STR));
-    ASSERT_EQ(PQPING_NO_RESPONSE, Connection::ping("port=2345"));
-    ASSERT_EQ(PQPING_NO_ATTEMPT, Connection::ping("k=v"));
-}
-
-TEST(TestConnection, PingUri) {
     ASSERT_EQ(PQPING_OK, Connection::ping(CONNECT_URI));
+
+    ASSERT_EQ(PQPING_NO_RESPONSE, Connection::ping(Config::Builder{}.port(2345).build()));
+    ASSERT_EQ(PQPING_NO_RESPONSE, Connection::ping("port=2345"));
     ASSERT_EQ(PQPING_NO_RESPONSE, Connection::ping("postgresql://:2345"));
+
+    ASSERT_EQ(PQPING_NO_ATTEMPT, Connection::ping(Config::Builder{}.set("k", "v").build()));
+    ASSERT_EQ(PQPING_NO_ATTEMPT, Connection::ping("k=v"));
     ASSERT_EQ(PQPING_NO_ATTEMPT, Connection::ping("postgresql://?k=v"));
 }
 
@@ -33,84 +29,86 @@ TEST(TestConnection, Connect) {
     Connection conn{Config::build()};
     ASSERT_TRUE(conn.isOk());
     ASSERT_TRUE(conn.message().empty());
-    ASSERT_NE(nullptr, conn.native());
+    ASSERT_TRUE(conn.reset());
 }
 
 TEST(TestConnection, ConnectBad) {
     Connection conn{Config::Builder{}.port(2345).build()};
     ASSERT_FALSE(conn.isOk());
     ASSERT_FALSE(conn.message().empty());
-    ASSERT_NE(nullptr, conn.native());
+    ASSERT_FALSE(conn.reset());
 }
 
 TEST(TestConnection, ConnectStr) {
     Connection conn{CONNECT_STR};
     ASSERT_TRUE(conn.isOk());
     ASSERT_TRUE(conn.message().empty());
-    ASSERT_NE(nullptr, conn.native());
+    ASSERT_TRUE(conn.reset());
 }
 
 TEST(TestConnection, ConnectStrBad) {
     Connection conn{"port=2345"};
     ASSERT_FALSE(conn.isOk());
     ASSERT_FALSE(conn.message().empty());
-    ASSERT_NE(nullptr, conn.native());
+    ASSERT_FALSE(conn.reset());
 }
 
 TEST(TestConnection, ConnectUri) {
     Connection conn{CONNECT_URI};
     ASSERT_TRUE(conn.isOk());
     ASSERT_TRUE(conn.message().empty());
-    ASSERT_NE(nullptr, conn.native());
+    ASSERT_TRUE(conn.reset());
 }
 
 TEST(TestConnection, ConnectUriBad) {
     Connection conn{"postgresql://:2345"};
     ASSERT_FALSE(conn.isOk());
     ASSERT_FALSE(conn.message().empty());
-    ASSERT_NE(nullptr, conn.native());
-}
-
-TEST(TestConnection, Reset) {
-    Connection conn{Config::build()};
-    ASSERT_TRUE(conn.reset());
-    ASSERT_TRUE(conn.isOk());
+    ASSERT_FALSE(conn.reset());
 }
 
 TEST(TestConnection, Exec) {
     Connection conn{Config::build()};
     ASSERT_TRUE(conn.exec(Command{"SELECT 1"}).isOk());
-    ASSERT_TRUE(conn.send(Command{"SELECT 1"}).receive().isOk());
     ASSERT_FALSE(conn.exec(Command{"SELECT 1; SELECT 2"}).isOk());
-    ASSERT_FALSE(conn.send(Command{"SELECT 1; SELECT 2"}).receive().isOk());
     ASSERT_FALSE(conn.exec(Command{"BAD"}).isOk());
-    ASSERT_FALSE(conn.send(Command{"BAD"}).receive().isOk());
-
-    ASSERT_TRUE(conn.execRaw("SELECT 1").isOk());
-    ASSERT_TRUE(conn.sendRaw("SELECT 1").receive().isOk());
-    ASSERT_TRUE(conn.execRaw("SELECT 1; SELECT 2").isOk());
-    ASSERT_TRUE(conn.sendRaw("SELECT 1; SELECT 2").receive().isOk());
-    ASSERT_FALSE(conn.execRaw("BAD").isOk());
-    ASSERT_FALSE(conn.sendRaw("BAD").receive().isOk());
-
-    ASSERT_TRUE(conn.prepare(PrepareData{"select1", "SELECT 1"}).isOk());
-    ASSERT_TRUE(conn.prepareAsync(PrepareData{"select2", "SELECT 2"}).receive().isOk());
-    ASSERT_TRUE(conn.execPrepared(Command{"select1"}).isOk());
-    ASSERT_TRUE(conn.sendPrepared(Command{"select2"}).receive().isOk());
-    ASSERT_FALSE(conn.execPrepared(Command{"BAD"}).isOk());
-    ASSERT_FALSE(conn.sendPrepared(Command{"BAD"}).receive().isOk());
 }
 
-TEST(TestConnection, Broken) {
-    Connection conn{Config::Builder{}.port(2345).build()};
-    ASSERT_FALSE(conn.exec(Command{"SELECT 1"}).isOk());
-    ASSERT_FALSE(conn.send(Command{"SELECT 1"}).isOk());
-    ASSERT_FALSE(conn.execRaw("SELECT 1").isOk());
-    ASSERT_FALSE(conn.sendRaw("SELECT 1").isOk());
-    ASSERT_FALSE(conn.prepare(PrepareData{"select1", "SELECT 1"}).isOk());
-    ASSERT_FALSE(conn.execPrepared(Command{"select1"}).isOk());
-    ASSERT_FALSE(conn.prepareAsync(PrepareData{"select1", "SELECT 1"}).isOk());
-    ASSERT_FALSE(conn.sendPrepared(Command{"select1"}).isOk());
+TEST(TestConnection, ExecRaw) {
+    Connection conn{Config::build()};
+    ASSERT_TRUE(conn.execRaw("SELECT 1").isOk());
+    ASSERT_TRUE(conn.execRaw("SELECT 1; SELECT 2").isOk());
+    ASSERT_FALSE(conn.execRaw("BAD").isOk());
+}
+
+TEST(TestConnection, Prepare) {
+    Connection conn{Config::build()};
+    ASSERT_TRUE(conn.prepare(PrepareData{"select1", "SELECT 1"}).isOk());
+    ASSERT_TRUE(conn.execPrepared(Command{"select1"}).isOk());
+    ASSERT_FALSE(conn.prepare(PrepareData{"bad", "BAD"}).isOk());
+    ASSERT_FALSE(conn.execPrepared(Command{"bad"}).isOk());
+}
+
+TEST(TestConnection, ExecAsync) {
+    Connection conn{Config::build()};
+    ASSERT_TRUE(conn.send(Command{"SELECT 1"}).receive().isOk());
+    ASSERT_FALSE(conn.send(Command{"SELECT 1; SELECT 2"}).receive().isOk());
+    ASSERT_FALSE(conn.send(Command{"BAD"}).receive().isOk());
+}
+
+TEST(TestConnection, ExecRawAsync) {
+    Connection conn{Config::build()};
+    ASSERT_TRUE(conn.sendRaw("SELECT 1").receive().isOk());
+    ASSERT_TRUE(conn.sendRaw("SELECT 1; SELECT 2").receive().isOk());
+    ASSERT_FALSE(conn.sendRaw("BAD").receive().isOk());
+}
+
+TEST(TestConnection, PrepareAsync) {
+    Connection conn{Config::build()};
+    ASSERT_TRUE(conn.prepareAsync(PrepareData{"select1", "SELECT 1"}).receive().isOk());
+    ASSERT_TRUE(conn.sendPrepared(Command{"select1"}).receive().isOk());
+    ASSERT_FALSE(conn.prepareAsync(PrepareData{"bad", "BAD"}).receive().isOk());
+    ASSERT_FALSE(conn.sendPrepared(Command{"bad"}).receive().isOk());
 }
 
 TEST(TestConnection, Esc) {
